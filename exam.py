fc2ccb9cd22f9966366666075494d1c1a1b70150
idergunoff <idergunoff@yandex.ru>
2023-02-13 23:30:48 +0300

    exam_ege and exam_error don't show index of question, and show serial number
    Функции topic

--- exam.py
+++ exam.py
@@ -6,17 +6,71 @@ from func.func_user import get_admins, user_by_id
 @dp.callback_query_handler(cb_exam_subtopic.filter())
 @logger.catch
 async def start_exam_subtopic(call: types.CallbackQuery, callback_data: dict, state: FSMContext):
-    # todo проверка, есть ли начатый тест
     exam_id = await add_new_exam(call.from_user.id, 'sub')
     await create_tasks_sub_exam(call.from_user.id, callback_data['sub_id'], exam_id)
-    await TrainStates.EXAM.set()
     task = await get_task(call.from_user.id)
-    await state.update_data(quest_id=task.question_id, exam_id=exam_id, task_id=task.id)
-    quest = await question_by_id(task.question_id)
-    mes = f'Отправьте ответ на задание <b>{quest.subtopic.topic.index}.{quest.subtopic.index}.{quest.index}</b>'
-    await bot.send_photo(task.user_id, quest.link, caption=mes)
-    await start_task(task.id)
+    if task:
+        await TrainStates.EXAM.set()
+        await state.update_data(quest_id=task.question_id, exam_id=exam_id, task_id=task.id, num=1)
+        quest = await question_by_id(task.question_id)
+        mes = f'Отправьте ответ на задание <b>№ 1</b>'
+        await bot.send_photo(task.user_id, quest.link, caption=mes)
+        await start_task(task.id)
+    else:
+        await bot.send_message(call.from_user.id, 'В данной подтеме пока нет ни одного вопроса.')
     await call.answer()
+
+
+@dp.callback_query_handler(cb_exam_topic.filter())
+@logger.catch
+async def start_exam_topic(call: types.CallbackQuery, callback_data: dict, state: FSMContext):
+    exam_id = await add_new_exam(call.from_user.id, 'top')
+    await create_tasks_top_exam(call.from_user.id, callback_data['topic_id'], exam_id)
+    task = await get_task(call.from_user.id)
+    if task:
+        await TrainStates.EXAM.set()
+        await state.update_data(quest_id=task.question_id, exam_id=exam_id, task_id=task.id, num=1)
+        quest = await question_by_id(task.question_id)
+        mes = f'Отправьте ответ на задание <b>№ 1</b>'
+        await bot.send_photo(task.user_id, quest.link, caption=mes)
+        await start_task(task.id)
+    else:
+        await bot.send_message(call.from_user.id, 'В данной теме пока нет ни одного вопроса.')
+    await call.answer()
+
+
+@dp.message_handler(text=emojize('Работа над ошибками:scissors:'))
+@logger.catch
+async def start_exam_error(msg: types.Message, state: FSMContext):
+    exam_id = await add_new_exam(msg.from_user.id, 'error')
+    await create_tasks_error_exam(msg.from_user.id, exam_id)
+    task = await get_task(msg.from_user.id)
+    if task:
+        await TrainStates.EXAM.set()
+        await state.update_data(quest_id=task.question_id, exam_id=exam_id, task_id=task.id, num=1)
+        quest = await question_by_id(task.question_id)
+        mes = f'Отправьте ответ на задание <b>№ 1</b>'
+        await bot.send_photo(task.user_id, quest.link, caption=mes)
+        await start_task(task.id)
+    else:
+        await msg.reply('Поздравляю! У вас не осталось заданий с ошибками. Выберите другой режим тестирования.')
+
+
+@dp.message_handler(text=emojize('Экзамен:bullseye:'))
+@logger.catch
+async def start_exam_ege(msg: types.Message, state: FSMContext):
+    exam_id = await add_new_exam(msg.from_user.id, 'exam')
+    await create_tasks_exam_ege(msg.from_user.id, exam_id)
+    task = await get_task(msg.from_user.id)
+    if task:
+        await TrainStates.EXAM.set()
+        await state.update_data(quest_id=task.question_id, exam_id=exam_id, task_id=task.id, num=1)
+        quest = await question_by_id(task.question_id)
+        mes = f'Отправьте ответ на задание <b>№ 1</b>'
+        await bot.send_photo(task.user_id, quest.link, caption=mes)
+        await start_task(task.id)
+    else:
+        await msg.reply('В базе данных пока нет ни одного вопроса.')
 
 
 @dp.message_handler(state=TrainStates.EXAM)
@@ -25,14 +79,14 @@ async def accept_answer(msg: types.Message, state: FSMContext):
     user_data = await state.get_data()
     quest = await question_by_id(user_data['quest_id'])
     point = await check_answer(msg.text, quest, user_data['task_id'])
-    mes = f'Вы ответили <b>{msg.text}</b> на задание <b>{quest.subtopic.topic.index}.{quest.subtopic.index}.{quest.index}</b>' \
+    mes = f'Вы ответили <b>{msg.text}</b> на задание <b>№ {user_data["num"]} ({quest.subtopic.topic.index}.{quest.subtopic.index}.{quest.index})</b>' \
           f'\nЭто {"правильный" if point else "не правильный"} ответ.'
     await msg.reply(mes)
     task = await get_task(msg.from_user.id)
     if task:
-        await state.update_data(quest_id=task.question_id, task_id=task.id)
+        await state.update_data(quest_id=task.question_id, task_id=task.id, num=user_data['num'] + 1)
         quest = await question_by_id(task.question_id)
-        mes = f'Отправьте ответ на задание <b>{quest.subtopic.topic.index}.{quest.subtopic.index}.{quest.index}</b>'
+        mes = f'Отправьте ответ на задание <b>№ {user_data["num"] + 1}</b>'
         await bot.send_photo(task.user_id, quest.link, caption=mes)
         await start_task(task.id)
     else:
--- func/func_exam.py
+++ func/func_exam.py
@@ -29,6 +29,58 @@ async def create_tasks_sub_exam(t_id, sub_id, exam_id):
 
 
 @logger.catch
+async def create_tasks_top_exam(t_id, top_id, exam_id):
+    questions = session.query(Question).join(Subtopic).filter(Subtopic.topic_id == top_id, Question.showing == 1).all()
+    list_count_quest = []
+    for q in questions:
+        list_q = [session.query(Task).filter(Task.user_id == t_id, Task.question_id == q.id).count(), q.id]
+        list_count_quest.append(list_q)
+    random.shuffle(list_count_quest)
+    list_count_quest.sort(key=lambda x: x[0])
+    print(list_count_quest[:10])
+    for lq in list_count_quest[:10]:
+        new_task = Task(exam_id=exam_id, user_id=t_id, question_id=lq[1])
+        session.add(new_task)
+    session.commit()
+
+
+@logger.catch
+async def create_tasks_error_exam(t_id, exam_id):
+    questions = session.query(Question).filter(Question.showing == 1).all()
+    list_error_quest = []
+    for q in questions:
+        task = session.query(Task).filter(Task.user_id == t_id, Task.question_id == q.id).order_by(desc(Task.stop)).first()
+        if task:
+            if task.answer_point == 0:
+                list_error_quest.append(q.id)
+    random.shuffle(list_error_quest)
+    print(list_error_quest[:10])
+    for lq in list_error_quest[:10]:
+        new_task = Task(exam_id=exam_id, user_id=t_id, question_id=lq)
+        session.add(new_task)
+    session.commit()
+
+
+@logger.catch
+async def create_tasks_exam_ege(t_id, exam_id):
+    list_ege_quest = []
+    for top in session.query(Topic).all():
+        questions = session.query(Question).join(Subtopic).filter(Subtopic.topic_id == top.id, Question.showing == 1).all()
+        list_count_quest = []
+        for q in questions:
+            list_q = [session.query(Task).filter(Task.user_id == t_id, Task.question_id == q.id).count(), q.id]
+            list_count_quest.append(list_q)
+        random.shuffle(list_count_quest)
+        list_count_quest.sort(key=lambda x: x[0])
+        if len(list_count_quest) > 0:
+            list_ege_quest.append(list_count_quest[0][1])
+    print(list_ege_quest)
+    for lq in list_ege_quest:
+        new_task = Task(exam_id=exam_id, user_id=t_id, question_id=lq)
+        session.add(new_task)
+    session.commit()
+
+@logger.catch
 async def get_task(t_id):
     return session.query(Task).filter(Task.user_id == t_id, Task.answer_text == '').first()
 
